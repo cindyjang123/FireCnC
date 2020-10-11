f122368070d93aa980ec6a32f1d13553abb61691
Cindy <tellsthetruth99@gmail.com>
2020-10-11 15:06:06 -0400

    fixed server.js

--- script.js
+++ script.js
@@ -6,38 +6,38 @@ function onHttpStart() {
   console.log(`Express http server listening on ${HTTP_PORT}`);
 }
 
-app.use(express.static(path.join(__dirname + '/views')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
-app.get('/room', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/room.html'));
-});
-
-app.get('/book', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/booknow.html'));
-});
-
-app.get('/confirm', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/confirmation.html'));
-});
+app.use(express.static(`${__dirname}/views`));
 
 // app.get('/', (req, res) => {
-//   res.sendFile(`${__dirname}/views/index.html`);
+//   res.sendFile(path.join(__dirname, 'views/index.html'));
 // });
 
 // app.get('/room', (req, res) => {
-//   res.sendFile(`${__dirname}/views/room.html`);
+//   res.sendFile(path.join(__dirname, 'views/room.html'));
 // });
 
 // app.get('/book', (req, res) => {
-//   res.sendFile(`${__dirname}/views/booknow.html`);
+//   res.sendFile(path.join(__dirname, 'views/booknow.html'));
 // });
 
 // app.get('/confirm', (req, res) => {
-//   res.sendFile(`${__dirname}/views/confirmation.html`);
+//   res.sendFile(path.join(__dirname, 'views/confirmation.html'));
 // });
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get('/room', (req, res) => {
+  res.sendFile(`${__dirname}/views/room.html`);
+});
+
+app.get('/book', (req, res) => {
+  res.sendFile(`${__dirname}/views/booknow.html`);
+});
+
+app.get('/confirm', (req, res) => {
+  res.sendFile(`${__dirname}/views/confirmation.html`);
+});
 
 app.listen(HTTP_PORT, onHttpStart);
