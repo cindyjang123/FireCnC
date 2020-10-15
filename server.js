abf51f2486b8710ceaab1fa892997fc87e5dc1ea
cindyjang123 <sjang15@myseneca.ca>
2020-10-14 22:35:09 -0400

    server js fixed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const HTTP_PORT = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
-
+var path = require('path');
 var multer = require('multer');
 var nodemailer = require('nodemailer');
 
@@ -26,7 +26,7 @@ function onHttpStart() {
   console.log(`Express http server listening on ${HTTP_PORT}`);
 }
 
-app.use(express.static(`${__dirname}/views`));
+app.use(express.static(path.join(__dirname, '/views')));
 
 // app.get('/', (req, res) => {
 //   res.sendFile(`${__dirname}/views/index.html`);
@@ -60,47 +60,47 @@ app.get('/confirm', function (req, res) {
   res.sendFile(path.join(__dirname, '/views/confirmation.html'));
 });
 
-app.post('/contact-form-process', UPLOAD.single('photo'), (req, res) => {
-  const FORM_DATA = req.body;
-  const FORM_FILE = req.file;
+// app.post('/contact-form-process', UPLOAD.single('photo'), (req, res) => {
+//   const FORM_DATA = req.body;
+//   const FORM_FILE = req.file;
 
-  const DATA_RECEIVED =
-    'Your submission was received: <br/><br/>' +
-    'Your form data was:<br/>' +
-    JSON.stringify(FORM_DATA) +
-    '<br/><br/>' +
-    'Your file data was:<br/>' +
-    JSON.stringify(FORM_FILE) +
-    '<br/><p>This was the image just uploaded:<br/>' +
-    "<img src='/photos/" +
-    FORM_FILE.filename +
-    "'/>" +
-    '<br/><br/>Welcome <strong>' +
-    FORM_DATA.fname +
-    ' ' +
-    FORM_DATA.lname +
-    '</strong>' +
-    ' to the world of form processing.';
+//   const DATA_RECEIVED =
+//     'Your submission was received: <br/><br/>' +
+//     'Your form data was:<br/>' +
+//     JSON.stringify(FORM_DATA) +
+//     '<br/><br/>' +
+//     'Your file data was:<br/>' +
+//     JSON.stringify(FORM_FILE) +
+//     '<br/><p>This was the image just uploaded:<br/>' +
+//     "<img src='/photos/" +
+//     FORM_FILE.filename +
+//     "'/>" +
+//     '<br/><br/>Welcome <strong>' +
+//     FORM_DATA.fname +
+//     ' ' +
+//     FORM_DATA.lname +
+//     '</strong>' +
+//     ' to the world of form processing.';
 
-  var mailOptions = {
-    from: 'Web322Clint@gmail.com',
-    to: FORM_DATA.email,
-    subject: 'Test email from NODE.js using nodemailer',
-    html:
-      '<p>Hello ' +
-      FORM_DATA.fname +
-      ':</p><p>Thank-you for contacting us.</p>',
-  };
+//   var mailOptions = {
+//     from: 'Web322Clint@gmail.com',
+//     to: FORM_DATA.email,
+//     subject: 'Test email from NODE.js using nodemailer',
+//     html:
+//       '<p>Hello ' +
+//       FORM_DATA.fname +
+//       ':</p><p>Thank-you for contacting us.</p>',
+//   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log('ERROR: ' + error);
-    } else {
-      console.log('SUCCESS: ' + info.response);
-    }
-  });
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log('ERROR: ' + error);
+//     } else {
+//       console.log('SUCCESS: ' + info.response);
+//     }
+//   });
 
-  res.send(DATA_RECEIVED);
-});
+//   res.send(DATA_RECEIVED);
+// });
 
 app.listen(HTTP_PORT, onHttpStart);
