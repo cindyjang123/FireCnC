386d895ced57c1d091f8610f7368e6d49becdb47
cindyjang123 <sjang15@myseneca.ca>
2020-10-19 23:24:40 -0400

    changes

--- views/script.js
+++ views/script.js
@@ -1,26 +1,9 @@
-function addHTML() {
-  var z, i, element, file, xhttp;
-  z = document.getElementsByTagName('*');
-  for (i = 0; i < z.length; i++) {
-    element = z[i];
-    file = element.getAttribute('w3-include-html');
-    if (file) {
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            element.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            element.innerHTML = 'Page not found.';
-          }
-          element.removeAttribute('w3-include-html');
-          includeHTML();
-        }
-      };
-      xhttp.open('GET', file, true);
-      xhttp.send();
-      return;
-    }
-  }
+function visible() {
+  document.getElementById('hidden').style.visibility = 'visible';
+  document.getElementById('hidden').style.position = 'static';
+}
+
+function hide() {
+  document.getElementById('hidden').style.visibility = 'hidden';
+  document.getElementById('hidden').style.position = 'absolute';
 }
